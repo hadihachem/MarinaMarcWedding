e27493ab1c0708551b602358e2118dae843a8911
hadihachem <hadi.eliashachem@gmail.com>
2024-05-05 17:59:05 +0300

    fixed basename

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="WeddingMarinaMarc">
         <Header />
         <Navbar />
         <Routes>
